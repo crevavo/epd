d8fb41f1ea95814f602c7ee09e9a944efbe5f408
Hyougo Yuuki <yuuki.hyougo@gmail.com>
2023-05-01 22:08:59 +0900

    来客の時間はCalenderを非表示に

--- src/googlecal.py
+++ src/googlecal.py
@@ -1,9 +1,55 @@
-import os, datetime, pytz, json
+import os, datetime, pytz, json, pprint
 
 import googleapiclient.discovery
 import google.auth
 
 import setting as ss
+
+def getRequiredData(eventsList, calenderName):
+    events = []
+
+    calName = eventsList['summary']
+    for ev in eventsList['items']:
+        # 公開/非公開チェック
+        if 'summary' in ev:
+            title = ev['summary']
+            if title.startswith('※'):
+                continue
+        else:
+            title = 'Private'
+
+        # 終日チェック
+        if 'dateTime' in ev['start']:
+            est = datetime.datetime.fromisoformat(ev['start']['dateTime'])
+            eed = datetime.datetime.fromisoformat(ev['end']['dateTime'])
+            isAllDay = False
+        else:
+            est = datetime.datetime.fromisoformat(ev['start']['date'])
+            eed = datetime.datetime.fromisoformat(ev['end']['date'])
+            isAllDay = True
+        
+        # 時間TimeZone調整
+        jst = pytz.timezone('Asia/Tokyo')
+        if 'timeZone' not in ev['start']:
+            est = jst.localize(est)
+        if 'timeZone' not in ev['end']:
+            eed = jst.localize(eed)
+
+        # 場所調整
+        loc = ev['location'] if 'location' in ev else ''
+
+        d = {
+                'calendar': calName,
+                'title': title,
+                'start': est,
+                'end': eed,
+                'isAllDay': isAllDay,
+                'location': loc
+        }
+        events.append(d)
+
+    return events
+
 
 def getEvents():
     SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -32,51 +78,28 @@ def getEvents():
         if ss.debug:
             print(json.dumps(eventlist, ensure_ascii=False, indent=4))
 
-        calName = eventlist['summary']
-        for ev in eventlist['items']:
-            # 公開/非公開チェック
-            if 'summary' in ev:
-                title = ev['summary']
-                if title.startswith('※'):
-                    continue
-            else:
-                title = 'Private'
-
-            # 終日チェック
-            if 'dateTime' in ev['start']:
-                est = datetime.datetime.fromisoformat(ev['start']['dateTime'])
-                eed = datetime.datetime.fromisoformat(ev['end']['dateTime'])
-                isAllDay = False
-            else:
-                est = datetime.datetime.fromisoformat(ev['start']['date'])
-                eed = datetime.datetime.fromisoformat(ev['end']['date'])
-                isAllDay = True
-            
-            # 時間TimeZone調整
-            jst = pytz.timezone('Asia/Tokyo')
-            if 'timeZone' not in ev['start']:
-                est = jst.localize(est)
-            if 'timeZone' not in ev['end']:
-                eed = jst.localize(eed)
-
-            # 場所調整
-            loc = ev['location'] if 'location' in ev else ''
-
-            d = {
-                    'calendar': calName,
-                    'title': title,
-                    'start': est,
-                    'end': eed,
-                    'isAllDay': isAllDay,
-                    'location': loc
-            }
-            events.append(d)
+        el = getRequiredData(eventlist, calId)
+        events.extend(el)
 
     events = sorted(events, key=lambda x: x['start'])
     return events
 
 def draw(dr_imgBlk, dr_imgRed):
     events = getEvents()
+
+    # 来客モード Visitor mode
+    visitormode = ss.visitormode
+
+    jst = pytz.timezone('Asia/Tokyo')
+    dn = jst.localize(ss.dtnow)
+
+    for _e in events:
+        title = _e['title']
+        if title == '来客':
+            start = _e['start']
+            end = _e['end']
+            if start <= dn <= end:
+                visitormode = True
 
     for i in range(3):
         e = events[i]
@@ -88,11 +111,10 @@ def draw(dr_imgBlk, dr_imgRed):
         title = ss.limitTextLength(e['title'], 15)
         location = ss.limitTextLength(e['location'], 15)
 
-        # 来客モード Visitor mode
-        if ss.visitormode:
+        if visitormode:
             title = title[0] + "******"
             location = "*****"
-
+            
         dateStr = [start.day, start.strftime('%a'), e['calendar'][:2]]
         timeStr = ''
         if e['isAllDay']:
@@ -113,3 +135,6 @@ def draw(dr_imgBlk, dr_imgRed):
             dr_imgBlk.text((px + 50, py + 60), location, 0, ss.fontNS)
 
     return dr_imgBlk, dr_imgRed
+
+if __name__ == "__main__":
+    getEvents()
